7b2d8bb7296fb73c1341af1b6a77427834ba9988
Wai-Yin Kwan <wykhuh@icloud.com>
2017-07-15 10:11:25 -0700

    connect to api; output response

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
 var express = require('express');
+var axios = require('axios');
 var app = express();
+
+var env = process.env.NODE_ENV || 'development';
+// use dotenv in development
+if(env === 'development') {
+  console.log('boo')
+  require('dotenv').config()
+}
 
 app.set('view engine', 'ejs');
 
 app.get('/', function(req, res){
-  res.render('home');
+  axios.get(process.env.API_URL + 'users')
+  .then(function(response){
+    var users = response.data;
+    res.send(users)
+  })
+  .catch(function(err) {
+    console.log('ERROR:', err)
+  })
 })
 
 app.listen(process.env.PORT || 3000, function() {
