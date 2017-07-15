12c7195a5a8f2876ecb9580a5a2b2c77c579831c
Wai-Yin Kwan <wykhuh@icloud.com>
2017-07-15 10:19:48 -0700

    display list of users using template

--- app.js
+++ app.js
@@ -5,17 +5,20 @@ var app = express();
 var env = process.env.NODE_ENV || 'development';
 // use dotenv in development
 if(env === 'development') {
-  console.log('boo')
   require('dotenv').config()
 }
 
 app.set('view engine', 'ejs');
 
 app.get('/', function(req, res){
+  // connect to api
   axios.get(process.env.API_URL + 'users')
   .then(function(response){
+    // get data from api
     var users = response.data;
-    res.send(users)
+
+    // render template with api data
+    res.render('home', { users: users } )
   })
   .catch(function(err) {
     console.log('ERROR:', err)
